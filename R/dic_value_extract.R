4d8e584a590e0fb1961ab3b2d6fb5807a587a451
davidzgli <zhiguangli2020@gmail.com>
2021-02-25 09:51:35 -0500

    dic_value_extract_one_var function modifying
    The function dic_value_extract_one_var in dic_value_extract.R file was modified accordding to the suggestions of Dr. Launer after she inspected the dictionary of HAAS.
    1. Expand maximum to three column largest_1, _2, and _3
    2. Expand minimum to three column smallest_1, _2, and _3
    3. Add a new column for standard deviation
    4. Convert all columns to character using mutate + across

--- R/dic_value_extract.R
+++ R/dic_value_extract.R
@@ -154,43 +154,81 @@ dic_value_extract_one_dataset <- function(.data) {
 #'
 #'
 #'
+# Comment out the function Feb 24, 2021
+# dic_value_extract_one_var <- function(x, var_name) {
+#   # label = get_label(x)
+#   label = ifelse(is.null(sjlabelled::get_label(x)), NA, sjlabelled::get_label(x))
+#   value_distinct = as.character(dplyr::n_distinct(x))
+#   max = as.character(max(x, na.rm = T))
+#   min = as.character(min(x, na.rm = T))
+#   mean = as.character(mean(x, na.rm = T))
+#   if (length(table(x)) > 0) {
+#     top1_value = names(sort(table(x), decreasing = T)[1])
+#     top1_freq = as.character(sort(table(x), decreasing = T)[1])
+#     top2_value = names(sort(table(x), decreasing = T)[2])
+#     top2_freq = as.character(sort(table(x), decreasing = T)[2])
+#     top3_value = names(sort(table(x), decreasing = T)[3])
+#     top3_freq = as.character(sort(table(x), decreasing = T)[3])
+#   } else {
+#     top1_value = NA
+#     top1_freq = NA
+#     top2_value = NA
+#     top2_freq = NA
+#     top3_value = NA
+#     top3_freq = NA
+#   }
+#   data.frame(
+#     var_name,
+#     label,
+#     value_distinct,
+#     max,
+#     min,
+#     mean,
+#     top1_value,
+#     top1_freq,
+#     top2_value,
+#     top2_freq,
+#     top3_value,
+#     top3_freq
+#   )
+# }
+
+
 dic_value_extract_one_var <- function(x, var_name) {
-  # label = get_label(x)
-  label = ifelse(is.null(sjlabelled::get_label(x)), NA, sjlabelled::get_label(x))
-  value_distinct = as.character(dplyr::n_distinct(x))
-  max = as.character(max(x, na.rm = T))
-  min = as.character(min(x, na.rm = T))
-  mean = as.character(mean(x, na.rm = T))
-  if (length(table(x)) > 0) {
-    top1_value = names(sort(table(x), decreasing = T)[1])
-    top1_freq = as.character(sort(table(x), decreasing = T)[1])
-    top2_value = names(sort(table(x), decreasing = T)[2])
-    top2_freq = as.character(sort(table(x), decreasing = T)[2])
-    top3_value = names(sort(table(x), decreasing = T)[3])
-    top3_freq = as.character(sort(table(x), decreasing = T)[3])
-  } else {
-    top1_value = NA
-    top1_freq = NA
-    top2_value = NA
-    top2_freq = NA
-    top3_value = NA
-    top3_freq = NA
-  }
-  data.frame(
+  # sort decreasingly by frequency
+  frq <- sort(table(x), decreasing = T)
+  x_sorted <- sort(x, decreasing = TRUE)
+  # make the tibble, one row
+  tibble(
     var_name,
-    label,
-    value_distinct,
-    max,
-    min,
-    mean,
-    top1_value,
-    top1_freq,
-    top2_value,
-    top2_freq,
-    top3_value,
-    top3_freq
-  )
+    label = ifelse(
+      is.null(sjlabelled::get_label(x)),
+      NA,
+      sjlabelled::get_label(x)
+    ),
+    # value_distinct = as.character(dplyr::n_distinct(x)),
+    value_distinct = dplyr::n_distinct(x),
+    # max = as.character(max(x, na.rm = T)),
+    # min = as.character(min(x, na.rm = T)),
+    # mean = as.character(mean(x, na.rm = T)),
+    mean = mean(x, na.rm = T),
+    sd = sd(x, na.rm = T),
+    largest_1 = x_sorted[1],
+    largest_2 = x_sorted[2],
+    largest_3 = x_sorted[3],
+    smallest_1 = rev(x_sorted)[1],
+    smallest_2 = rev(x_sorted)[2],
+    smallest_3 = rev(x_sorted)[3],
+    largest_300 = x_sorted[300],
+    top1_value = ifelse(length(frq) == 0, NA, names(frq[1])),
+    top1_freq = ifelse(length(frq) == 0, NA, frq[1]),
+    top2_value = ifelse(length(frq) == 0, NA, names(frq[2])),
+    top2_freq = ifelse(length(frq) == 0, NA, frq[2]),
+    top3_value = ifelse(length(frq) == 0, NA, names(frq[3])),
+    top3_freq = ifelse(length(frq) == 0, NA, frq[3]),
+  ) %>% mutate(across(everything(), as.character))
 }
+
 
 
 #' the safe version of extract values from one variable
